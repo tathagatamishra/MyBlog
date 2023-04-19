f2cbde7af5d0fb7b0e97de4f564d9dd414f0a8cb
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-04-19 14:43:10 +0530

    minor bug fix

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,7 +1,8 @@
+
 const mongoose = require("mongoose")
 
-// const DB = process.env.DATABASE
-const DB = 'mongodb+srv://new_user:jk1BBWwmxQpZ31zO@cluster0.pxvwsjp.mongodb.net/myblog'
+const DB = process.env.DATABASE
+// const DB = 'mongodb+srv://new_user:jk1BBWwmxQpZ31zO@cluster0.pxvwsjp.mongodb.net/myblog'
 
 mongoose.connect(DB, {
     useUnifiedTopology: true,
--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ app.use(route);
 // app.use('/', route)
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server Run on ${PORT} ...`)
