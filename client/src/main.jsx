8dfbd68291f2ceebcdfa898c0e6b3b944264eab5
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-04-20 23:32:40 +0530

    bug fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./homepage/Home.jsx";
 import "./main.scss";
-import Navbar from "./components/Navbar";
+import Navbar from "./navbar/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
